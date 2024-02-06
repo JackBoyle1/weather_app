cb8a1ffdb1f42bbe48fb2fe4a6c5fa537d16d2e5
Jack <jackboyle35@gmail.com>
2024-02-06 22:10:22 +0000

    Update searchbar css

--- components/Location.vue
+++ components/Location.vue
@@ -1,23 +1,54 @@
 <template>
   <div>
     <div class="input-container">
-      <img src="@/assets/img/should_i_wear.png"/>
-      <input
-        v-model="localLocation"
-        @input="updateParentLocation"
-        @keydown.enter="submitSearch"
-        class="shadow appearance-none border rounded w-full text-center py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="location"
-        type="text"
-        placeholder="Please type out your location..."
-      />
-      <span class="search-icon pb-12" @click="submitSearch">&#128269;</span>
-      <img class="pt-11" src="@/assets/img/coat.png"/>
+      <img src="@/assets/img/should_i_wear.png" />
+      <div class="flex">
+        <label
+          for="search-dropdown"
+          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >Your Email</label
+        >
+        <div class="relative w-full">
+          <input
+            id="location"
+            v-model="localLocation"
+            @input="updateParentLocation"
+            @keydown.enter="submitSearch"
+            type="search"
+            class="block w-full px-4 py-2.5 text-sm text-gray-900 placeholder-gray-500 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-800"
+            placeholder="Please type out your location..."
+            required
+          />
+          <button
+            @click="submitSearch"
+            type="submit"
+            class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-gray-500 rounded-e-lg border border-gray-300 hover:bg-black focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+          >
+            <svg
+              class="w-4 h-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+            <span class="sr-only">Search</span>
+          </button>
+        </div>
+      </div>
+      <img class="pt-11" src="@/assets/img/coat.png" />
     </div>
   </div>
 </template>
-  
-  <script>
+
+<script>
 export default {
   props: {
     value: {
@@ -35,8 +66,8 @@ export default {
       this.$emit("input", this.localLocation);
     },
     submitSearch() {
-        this.$emit("submitSearch");
-    }
+      this.$emit("submitSearch");
+    },
   },
 };
 </script>
