17a89401ca49fa9ceebc50905892fb8314524ca8
Jack <jackboyle35@gmail.com>
2024-02-08 21:59:11 +0000

    include current temperature in conditions

--- pages/index.vue
+++ pages/index.vue
@@ -42,8 +42,6 @@ export default {
     return {
       loading: false,
       showSearch: true,
-      minTemp: null,
-      maxTemp: null,
       highestForecastedPrecip: null,
       lowestForecastedTemp: null,
       currentTemp: null,
@@ -63,17 +61,18 @@ export default {
       if (this.isError) {
         return "Oops! Something went wrong";
       } else {
-        return this.minTemp <= 10 ||
-        this.snow > 0 ||
-        this.conditions.includes("Rain") ||
-        this.precipitationProb >= 5
-        ? "Yep"
-        : "Nah, no need";
+        return this.feelsLike <= 10 ||
+          this.lowestForecastedTemp <= 10 ||
+          this.snow > 0 ||
+          this.conditions.includes("Rain") ||
+          this.precipitationProb >= 5
+          ? "Yep"
+          : "Nah";
       }
     },
     backgroundColour() {
       if (this.isError) {
-        return "bg-gray-300"
+        return "bg-gray-300";
       } else if (this.showSearch) {
         return "bg-indigo-200";
       } else if (this.isWearACoat === "Yep") {
@@ -93,7 +92,7 @@ export default {
         this.updateValues(response.data);
         this.updateComments();
       } catch (error) {
-        this.updateComments();
+        this.comments = [];
         this.isError = true;
         this.showSearch = false;
         this.loading = false;
@@ -103,10 +102,9 @@ export default {
       this.loading = false;
     },
     updateValues(data) {
-      this.highestForecastedPrecip = this.highestForecastedPrecipitationOdds(data);
+      this.highestForecastedPrecip =
+        this.highestForecastedPrecipitationOdds(data);
       this.lowestForecastedTemp = this.lowestForecastedTemperature(data);
-      this.minTemp = data.days[0].tempmin;
-      this.maxTemp = data.days[0].tempmax;
       this.currentTemp = data.currentConditions.temp;
       (this.windspeed = data.currentConditions.windspeed),
         (this.snow = data.currentConditions.snow),
@@ -118,19 +116,24 @@ export default {
 
       if (this.snow > 0) {
         this.comments.push("snowing");
+      } else if (this.feelsLike > 0 && this.feelsLike <= 10) {
+        this.comments.push("cold");
       } else if (
         this.lowestForecastedTemp > 0 &&
         this.lowestForecastedTemp <= 10
       ) {
         this.comments.push("cold");
-      } else if (this.lowestForecastedTemp <= 0) {
+      } else if (this.lowestForecastedTemp <= 0 || this.feelsLike <= 0) {
         this.comments.push("freezing");
       }
 
       if (this.conditions.includes("Rain")) {
         this.comments.push("raining");
       } else {
-        if (this.highestForecastedPrecip >= 5 && this.highestForecastedPrecip <= 50) {
+        if (
+          this.highestForecastedPrecip >= 5 &&
+          this.highestForecastedPrecip <= 50
+        ) {
           this.comments.push("might rain");
         } else if (this.highestForecastedPrecip > 50) {
           this.comments.push("will probably rain");
@@ -156,7 +159,7 @@ export default {
           highestPrecipProb = precipProb;
         }
       });
-      
+
       return highestPrecipProb;
     },
     lowestForecastedTemperature(data) {
@@ -183,8 +186,8 @@ export default {
     },
     goBack() {
       this.isError = false;
-      this.showSearch = true
-    }
+      this.showSearch = true;
+    },
   },
 };
 </script>
